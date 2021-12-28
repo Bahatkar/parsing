2d6e8e16364080535aa82608a42c19dc35973f19
Bahatkar <Arcanika11@mail.ru>
2021-12-29 00:12:37 +0300

    Scraping whole site, import to excel

--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module parsing
 
 go 1.17
 
-require github.com/gocolly/colly v1.2.0
+require (
+	github.com/360EntSecGroup-Skylar/excelize v1.4.1
+	github.com/gocolly/colly v1.2.0
+)
 
 require (
 	github.com/PuerkitoBio/goquery v1.8.0 // indirect
@@ -14,6 +17,7 @@ require (
 	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
 	github.com/golang/protobuf v1.3.1 // indirect
 	github.com/kennygrant/sanitize v1.2.4 // indirect
+	github.com/mohae/deepcopy v0.0.0-20170929034955-c48cc78d4826 // indirect
 	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca // indirect
 	github.com/temoto/robotstxt v1.1.2 // indirect
 	golang.org/x/net v0.0.0-20211209124913-491a49abca63 // indirect
--- parsing.go
+++ parsing.go
@@ -2,35 +2,156 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
+	"time"
 
+	"github.com/360EntSecGroup-Skylar/excelize"
 	"github.com/gocolly/colly"
 )
 
-func main() {
-	//gethttp("https://www.vprok.ru/catalog/4019/sladosti-i-sneki")
-	gethttp("https://www.vprok.ru/catalog/1304/ryba-i-moreprodukty")
+type Products struct {
+	id       string
+	bigCat   string
+	smallCat string
+	name     string
+	oldPrice string
+	discount string
+	newPrice string
+	unit     string
+	url      string
+	date     string
+	time     string
 }
 
-func gethttp(url string) {
+func main() {
+	var itemCount, sumcount int
+	uniqItems := make(map[string]string)
+	catalogue := getCatalogue()
+
+	f := excelize.NewFile()
+
+	for j := 0; j < len(catalogue); j++ {
+		for i := 1; ; i++ {
+			if i == 1 {
+				itemCount = gethttp(catalogue[j], f, uniqItems, &sumcount)
+				if itemCount < 1 {
+					break
+				}
+			} else {
+				itemCount = gethttp(fmt.Sprintf("%s?page=%d", catalogue[j], i), f, uniqItems, &sumcount)
+				if itemCount < 1 {
+					break
+				}
+			}
+			fmt.Printf("Страница %d раздела %s готова\n", i, catalogue[j])
+		}
+	}
+	fmt.Printf("Загружено товаров: %d\n", sumcount)
+
+	if err := f.SaveAs("Perik.xlsx"); err != nil {
+		fmt.Println(err)
+	}
+}
+
+func gethttp(uurl string, f *excelize.File, uniqItems map[string]string, sumCount *int) (itemCount int) {
+	var (
+		reqUrl    string
+		stopScrap bool
+	)
+	itemCount = 0
+
 	c := colly.NewCollector()
 
-	c.OnHTML("main > div> div > ul li", func(e *colly.HTMLElement) {
-		name := strings.TrimSpace(e.DOM.Find("div.xf-product__title").Text())
-		discount := strings.TrimSpace(e.DOM.Find("div.xf-product__cost > div.xf-product-cost__old-price > p.js-calculated-discount").Text())
-		if discount != "" {
-			discount += "%"
-		}
-		newPrice := strings.TrimSpace(e.DOM.Find("div.xf-product__cost > div.xf-price > span.xf-price__rouble").Text() +
-			e.DOM.Find("div.xf-product__cost > div.xf-price > span.xf-price__penny").Text())
-		oldPrice := strings.TrimSpace(e.DOM.Find("div.xf-product__cost > div.xf-product-cost__old-price > div.xf-price").Text())
-		unit := strings.TrimSpace(e.DOM.Find("div.xf-product__cost > div.xf-price > span.xf-price__unit").Text())
-		fmt.Println("Наименование: " + name)
-		fmt.Println("Цена без скидки: " + oldPrice)
-		fmt.Println("Размер скидки: " + discount)
-		fmt.Println("Цена со скидкой: " + newPrice)
-		fmt.Println("Количество: " + unit)
+	c.OnRequest(func(h *colly.Request) {
+		reqUrl = h.URL.String()
 	})
 
-	c.Visit(url)
+	c.OnResponse(func(g *colly.Response) {
+		if reqUrl != g.Request.URL.String() {
+			stopScrap = true
+			return
+		}
+	})
+
+	c.OnHTML(".js-catalog-product", func(e *colly.HTMLElement) {
+		pds := new(Products)
+
+		if stopScrap {
+			return
+		}
+
+		pds.id = strings.TrimSpace(e.DOM.Find(".xf-product").AttrOr("data-id", ""))
+		pds.bigCat = strings.TrimSpace((e.DOM.Find(".xf-product").AttrOr("data-owox-portal-name", "")))
+		pds.smallCat = strings.TrimSpace((e.DOM.Find(".xf-product").AttrOr("data-owox-category-name", "")))
+		pds.name = strings.TrimSpace(e.DOM.Find(".xf-product-title__link").Text())
+		if pds.name != "" {
+			itemCount++
+		}
+		pds.discount = strings.TrimSpace(e.DOM.Find(".js-calculated-discount").Text())
+		if pds.discount != "" {
+			pds.discount += "%"
+		}
+		pds.newPrice = strings.TrimSpace(e.DOM.Find("div.xf-price.xf-product-cost__current.js-product__cost").AttrOr("data-cost", ""))
+		pds.oldPrice = strings.TrimSpace(e.DOM.Find("div.xf-price.xf-product-cost__prev.js-product__old-cost").AttrOr("data-cost", ""))
+		pds.unit = strings.TrimSpace(e.DOM.Find(".js-fraction-text").Text())
+		pds.url = strings.TrimSpace(e.DOM.Find(".xf-product").AttrOr("data-product-card-url", ""))
+		if pds.bigCat == "Зоотовары" {
+			pds.url = fmt.Sprintf("https://zoo.vprok.ru" + pds.url)
+		} else {
+			pds.url = fmt.Sprintf("https://www.vprok.ru" + pds.url)
+		}
+		pds.date = time.Now().Format("02.01.2006")
+		pds.time = time.Now().Format("15:04:05")
+
+		if pds.mapChecking(uniqItems) {
+			*sumCount++
+			excelWriting(f, pds, *sumCount)
+			fmt.Printf("Собран товар: %v\n", pds.name)
+		}
+	})
+
+	c.Visit(uurl)
+
+	return
+}
+
+func getCatalogue() []string {
+	var catalogue = make([]string, 0)
+	c := colly.NewCollector()
+
+	c.OnHTML("div.xf-catalog-categories__col._right li", func(e *colly.HTMLElement) {
+		href, ok := e.DOM.Find(".xf-catalog-categories__link").Attr("href")
+		if ok && strings.TrimSpace(href) == "/catalog/1308/tovary-dlya-jivotnyh" {
+			catalogue = append(catalogue, fmt.Sprintf("https://zoo.vprok.ru"+strings.TrimSpace(href)))
+		} else if ok {
+			catalogue = append(catalogue, fmt.Sprintf("https://www.vprok.ru"+strings.TrimSpace(href)))
+		}
+	})
+
+	c.Visit("https://www.vprok.ru/catalog")
+
+	return catalogue
+}
+
+func (pds *Products) compound() (cmp []string) {
+	return []string{pds.id, pds.bigCat, pds.smallCat, pds.name, pds.oldPrice, pds.discount, pds.newPrice, pds.unit, pds.url,
+		pds.date, pds.time}
+}
+
+func excelWriting(book *excelize.File, pds *Products, sumcount int) {
+	var r rune = 64
+	b := pds.compound()
+	for i := 1; i < len(b)+1; i++ {
+		book.SetCellValue("Sheet1", fmt.Sprintf("%c"+strconv.Itoa(sumcount+1), r+int32(i)), b[i-1])
+	}
+}
+
+func (pds *Products) mapChecking(uniqItems map[string]string) (ok bool) {
+	if _, ok := (uniqItems)[pds.id]; ok {
+		return !ok
+	} else {
+		(uniqItems)[pds.id] = pds.name
+	}
+	return !ok
 }
